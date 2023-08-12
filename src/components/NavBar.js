71d98992b77dfa4cf86ac44f9619361654e3062f
ymmy2 <oleg.nosyrev@itential.com>
2023-08-11 18:40:10 -0700

    removing memo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/css/entry.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
